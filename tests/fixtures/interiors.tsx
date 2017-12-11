c57a3fe3e27d544eb2cf5cc24b24af9e4f7404bb
Bastien Sevajol <sevajol.bastien@gmail.com>
2017-12-11 18:23:52 +0100

    Add Interior management (back only)

---
+++ tests/fixtures/interiors.tsx
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="interiors" tilewidth="8" tileheight="8" spacing="1" tilecount="49" columns="7">
+ <image source="interiors.png" width="64" height="64"/>
+ <tile id="0">
+  <properties>
+   <property name="name" type="str" value="Exterior"/>
+   <property name="id" type="str" value="ext"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="name" type="str" value="interior"/>
+   <property name="id" type="str" value="int"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="name" type="str" value="separator"/>
+   <property name="id" type="str" value="sep"/>
+  </properties>
+ </tile>
+</tileset>
---
+++ tests/gui/test_interiors.py
@@ -0,0 +1,130 @@
+# coding: utf-8
+from synergine2_xyz.map import TMXMap
+
+from opencc.simulation.interior import InteriorManager
+
+
+def test_interior_zones__one_zone():
+    map_ = TMXMap('tests/fixtures/one_interior.tmx')
+    manager = InteriorManager(map_)
+    interiors = manager.get_interiors()
+
+    assert interiors
+    assert isinstance(interiors, list)
+    assert 1 == len(interiors)
+
+    assert (1, 1) in interiors[0]
+    assert (1, 2) in interiors[0]
+    assert (1, 3) in interiors[0]
+    assert (2, 1) in interiors[0]
+    assert (2, 2) in interiors[0]
+    assert (2, 3) in interiors[0]
+    assert (3, 1) in interiors[0]
+    assert (3, 2) in interiors[0]
+    assert (3, 3) in interiors[0]
+    assert 9 == len(interiors[0])
+
+
+def test_interior_zones__two_separated_zones():
+    map_ = TMXMap('tests/fixtures/two_interiors.tmx')
+    manager = InteriorManager(map_)
+    interiors = sorted(manager.get_interiors())
+
+    assert interiors
+    assert isinstance(interiors, list)
+    assert 2 == len(interiors)
+
+    assert (0, 1) in interiors[0]
+    assert (1, 1) in interiors[0]
+    assert (0, 2) in interiors[0]
+    assert (1, 2) in interiors[0]
+    assert (0, 3) in interiors[0]
+    assert (1, 3) in interiors[0]
+    assert 6 == len(interiors[0])
+
+    assert (3, 1) in interiors[1]
+    assert (4, 1) in interiors[1]
+    assert (3, 2) in interiors[1]
+    assert (4, 2) in interiors[1]
+    assert (3, 3) in interiors[1]
+    assert (4, 3) in interiors[1]
+    assert 6 == len(interiors[1])
+
+
+def test_interiors_zones__side_by_side_zones_with_separator():
+    map_ = TMXMap('tests/fixtures/side_by_side_interiors.tmx')
+    manager = InteriorManager(map_)
+    interiors = sorted(manager.get_interiors())
+
+    assert interiors
+    assert isinstance(interiors, list)
+    assert 2 == len(interiors)
+
+    assert (0, 1) in interiors[0]
+    assert (1, 1) in interiors[0]
+    assert (0, 2) in interiors[0]
+    assert (1, 2) in interiors[0]
+    assert (0, 3) in interiors[0]
+    assert (1, 3) in interiors[0]
+    assert (2, 1) in interiors[0]
+    assert (2, 2) in interiors[0]
+    assert (2, 3) in interiors[0]
+    assert 9 == len(interiors[0])
+
+    assert (2, 1) in interiors[1]
+    assert (2, 2) in interiors[1]
+    assert (2, 3) in interiors[1]
+    assert (3, 1) in interiors[1]
+    assert (4, 1) in interiors[1]
+    assert (3, 2) in interiors[1]
+    assert (4, 2) in interiors[1]
+    assert (3, 3) in interiors[1]
+    assert (4, 3) in interiors[1]
+    assert 9 == len(interiors[1])
+
+
+def test_interiors_zones__active_zones():
+    # active zones are zone where someone is in
+    map_ = TMXMap('tests/fixtures/two_interiors.tmx')
+    manager = InteriorManager(map_)
+
+    interiors = manager.get_interiors(where_positions=[(0, 1)])
+
+    assert (0, 1) in interiors[0]
+    assert (1, 1) in interiors[0]
+    assert (0, 2) in interiors[0]
+    assert (1, 2) in interiors[0]
+    assert (0, 3) in interiors[0]
+    assert (1, 3) in interiors[0]
+    assert 6 == len(interiors[0])
+
+    interiors = manager.get_interiors(where_positions=[(4, 2)])
+
+    assert 1 == len(interiors)
+    assert (3, 1) in interiors[0]
+    assert (4, 1) in interiors[0]
+    assert (3, 2) in interiors[0]
+    assert (4, 2) in interiors[0]
+    assert (3, 3) in interiors[0]
+    assert (4, 3) in interiors[0]
+    assert 6 == len(interiors[0])
+
+    interiors = manager.get_interiors(where_positions=[(0, 1), (4, 2)])
+
+    assert 2 == len(interiors)
+
+    assert (0, 1) in interiors[0]
+    assert (1, 1) in interiors[0]
+    assert (0, 2) in interiors[0]
+    assert (1, 2) in interiors[0]
+    assert (0, 3) in interiors[0]
+    assert (1, 3) in interiors[0]
+    assert 6 == len(interiors[0])
+
+    assert (3, 1) in interiors[1]
+    assert (4, 1) in interiors[1]
+    assert (3, 2) in interiors[1]
+    assert (4, 2) in interiors[1]
+    assert (3, 3) in interiors[1]
+    assert (4, 3) in interiors[1]
+    assert 6 == len(interiors[1])
