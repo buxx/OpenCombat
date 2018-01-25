1eb8a1625bf044e940cbae45ad4f425768a3c107
Bastien Sevajol <sevajol.bastien@gmail.com>
2018-01-25 20:40:17 +0100

    set subject behavior selector

--- opencombat/simulation/subject.py
+++ opencombat/simulation/subject.py
@@ -1,4 +1,8 @@
 # coding: utf-8
+import typing
+
+from synergine2.simulation import SubjectBehaviourSelector, SubjectBehaviour
+
 from opencombat.const import COLLECTION_ALIVE
 from opencombat.const import COMBAT_MODE_DEFENSE
 from opencombat.simulation.base import BaseSubject
@@ -6,6 +10,14 @@ from opencombat.simulation.behaviour import MoveToBehaviour
 from opencombat.simulation.behaviour import EngageOpponent
 from opencombat.simulation.behaviour import LookAroundBehaviour
 from synergine2.share import shared
+
+
+class TileBehaviourSelector(SubjectBehaviourSelector):
+    def reduce_behaviours(
+        self,
+        behaviours: typing.Dict[typing.Type[SubjectBehaviour], object],
+    ) -> typing.Dict[typing.Type[SubjectBehaviour], object]:
+        return behaviours
 
 
 class TileSubject(BaseSubject):
@@ -19,5 +31,4 @@ class TileSubject(BaseSubject):
     ]
     visible_opponent_ids = shared.create_self('visible_opponent_ids', lambda: [])
     combat_mode = shared.create_self('combat_mode', COMBAT_MODE_DEFENSE)
-    # TODO: implement (copied from engulf)
-    # behaviour_selector_class = CellBehaviourSelector
+    behaviour_selector_class = TileBehaviourSelector
