7483fbbb5be8d12da0c405b421723373f9021396
Bastien Sevajol <bastien.sevajol@algoo.fr>
2023-03-23 18:19:29 +0100

    ci: build on tag for windows and linux

---
+++ assets/linux/sidea__embedded_server.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+./battle_gui --embedded-server --server-rep-address tcp://0.0.0.0:4255 --server-bind-address tcp://0.0.0.0:4256 --side a --side-a-control W --side-a-control NW --side-a-control SW --side-b-control ALL
---
+++ assets/w64/sideb__client.bat
@@ -0,0 +1,2 @@
+if not defined in_subprocess (cmd /k set in_subprocess=y ^& %0 %*) & exit )
+battle_gui.exe --server-rep-address tcp://0.0.0.0:4255 --server-bind-address tcp://0.0.0.0:4256 --side b --side-a-control W --side-a-control NW --side-a-control SW --side-b-control ALL
