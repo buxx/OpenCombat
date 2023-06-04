0185db42c333a526d3f7849b242da98175080ae6
Bastien Sevajol <bastien.sevajol@algoo.fr>
2023-06-04 18:28:16 +0200

    Add demo1

---
+++ resources/maps/Demo1/trees.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="trees" tilewidth="64" tileheight="64" tilecount="20" columns="10">
+ <image source="trees.png" width="640" height="128"/>
+</tileset>
