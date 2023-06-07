8f669d28e96e23c64e27aa441990b4c244a279d4
Bastien Sevajol <bastien.sevajol@algoo.fr>
2023-06-07 20:31:00 +0200

    feat: adapt assets for demo1

--- assets/linux/sideb__client.sh
+++ assets/linux/sideb__client.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-battle_gui map1 --server-rep-address tcp://0.0.0.0:4255 --server-bind-address tcp://0.0.0.0:4256 --side b --side-a-control W --side-a-control NW --side-a-control SW --side-b-control ALL
+battle_gui Demo1 assets/demo1_deployment.json --server-rep-address tcp://0.0.0.0:4255 --server-bind-address tcp://0.0.0.0:4256 --side b --side-a-control N --side-a-control NW --side-a-control W --side-b-control ALL
--- assets/w64/sidea__embedded_server.bat
+++ assets/w64/sidea__embedded_server.bat
@@ -1,2 +1,2 @@
 if not defined in_subprocess (cmd /k set in_subprocess=y ^& %0 %*) & exit )
-battle_gui.exe map1 --embedded-server --server-rep-address tcp://0.0.0.0:4255 --server-bind-address tcp://0.0.0.0:4256 --side a --side-a-control W --side-a-control NW --side-a-control SW --side-b-control ALL
+battle_gui.exe Demo1 assets/demo1_deployment.json --embedded-server --server-rep-address tcp://0.0.0.0:4255 --server-bind-address tcp://0.0.0.0:4256 --side a --side-a-control N --side-a-control NW --side-a-control W --side-b-control ALL
