e133d00c9d929afafe5e6165ae1fa8d64faf3f5d
Bastien Sevajol <bastien.sevajol@algoo.fr>
2023-08-23 14:07:52 +0200

    chore: code quality

--- battle_server/src/runner/gesture/idle.rs
+++ battle_server/src/runner/gesture/idle.rs
@@ -14,7 +14,10 @@ impl Runner {
     pub fn idle_gesture(&self, soldier: &Soldier) -> GestureResult {
         if let Some(opponent) = self.soldier_find_opponent_to_target(soldier, None) {
             let point = opponent.world_point();
-            if let Some(_) = self.soldier_able_to_fire_on_point(soldier, &point) {
+            if self
+                .soldier_able_to_fire_on_point(soldier, &point)
+                .is_some()
+            {
                 return GestureResult::SwitchToBehavior(Behavior::EngageSoldier(opponent.uuid()));
             }
         }
